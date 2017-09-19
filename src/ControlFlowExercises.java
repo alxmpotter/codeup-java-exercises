3ba1485fdb3dc166e9b48f7bc0fcfab53c2b9bee
alxmpotter <alxmpotter@gmail.com>
2017-09-19 14:31:48 -0500

    Control Flow Exercise
    -change do while loop to count backwards by 5 from 100 to -10

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,18 +1,21 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+////////while loop count form 5 to 15
+//
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
 
+///////do while loop, count backwards from 5
 
-        System.out.println("Count by two:");
-        int evenNumber = 0;
+       int i = 100;
 
-        do {
-            System.out.println(evenNumber);
-            evenNumber = evenNumber + 2;
-        } while (evenNumber <= 100);
+       do{
+           System.out.println(i);
+           i = i - 5;
+       } while(i >= -10);
+
     }
 }
